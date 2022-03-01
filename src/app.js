e009687a59ae7a2aa7f1a8a462d3e74be72707ed
unknown <jmcc986@gmail.com>
2022-03-01 09:59:26 -0500

    Added other files for ejs, still pre express but forgot to do npm init

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@
 const app = require("express");
 const dotenv = require("dotenv");
 
+// View Engine
+app.set("view engine", "ejs");
 // Import Routes
-
+const index = require("./routes/index");
 // Use Routes
+app.use(index);
+
+// Run App
+app.listen(PORT, () => {
+  console.log(`App listening in ${process.env.NODE_ENV} on port ${PORT}`);
+});
