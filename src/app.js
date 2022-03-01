bb00ab1c52741f4ea32cd30757a7c36a7d8810be
unknown <jmcc986@gmail.com>
2022-03-01 11:11:20 -0500

    Revert "Added other files for ejs, still pre express but forgot to do npm init"
    This reverts commit e009687a59ae7a2aa7f1a8a462d3e74be72707ed.

--- src/app.js
+++ src/app.js
@@ -2,14 +2,6 @@
 const app = require("express");
 const dotenv = require("dotenv");
 
-// View Engine
-app.set("view engine", "ejs");
 // Import Routes
-const index = require("./routes/index");
-// Use Routes
-app.use(index);
 
-// Run App
-app.listen(PORT, () => {
-  console.log(`App listening in ${process.env.NODE_ENV} on port ${PORT}`);
-});
+// Use Routes
