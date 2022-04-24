f9352cf80181b911056b76a85662d8b715e5c4ce
unknown <jmcc986@gmail.com>
2022-04-24 00:15:44 -0400

    Added administrative route with appointment data

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -26,3 +26,23 @@ exports.createAppointment = async (req, res, next) => {
   const appointment = await Appointment.create(req.body);
   res.redirect("/schedule&booking");
 };
+
+exports.viewAppointments = async (req, res, next) => {
+  //console.log(req.user.role);
+  if (req.user.role !== process.env.AUTHORIZED) {
+    res.redirect("/");
+  }
+  const url = process.env.APPOINTMENTS;
+  // Send request to API for information upon hitting route
+  const response = await axios
+    .get(url, {})
+    .then((data) => data.data.data)
+    .then(
+      (data) => {
+        res.render("appointments", { user: req.user, data: data });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+};
--- routes/appointments.js
+++ routes/appointments.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const {
   newAppointment,
   createAppointment,
+  viewAppointments,
 } = require("../controllers/appointments");
 
+router.route("/view").get(viewAppointments);
 router.route("/new").get(newAppointment);
 router.route("/new").post(createAppointment);
 
