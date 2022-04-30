e5fa5034dd69f6da9e565008afe6e8f200400d3c
unknown <jmcc986@gmail.com>
2022-04-30 19:05:10 -0400

    Fixed islogged, started conditional rendering for calendar

--- routes/confirmed.js
+++ routes/confirmed.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getConfirmed, createConfirmed } = require("../controllers/confirmed");
-const { isLogged } = require("../middleware/islogged");
+const isLogged = require("../middleware/islogged");
 
 router.route("/").get(isLogged, getConfirmed).post(createConfirmed);
+router.route("/").post(createConfirmed);
 
 module.exports = router;
